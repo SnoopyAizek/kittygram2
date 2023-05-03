ca9153b9b10a8caec37b3566e9839505998f18f9
Andrei <kr4806@yandex.ru>
2023-05-03 16:26:10 +0400

    Добавлена фильтрация, пагинация, разрешение на для владельцев и не владельцев котиков

--- cats/views.py
+++ cats/views.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .permissions import OwnerOrReadOnly, ReadOnly
+from .paginations import CatsPagination
 from .models import Achievement, Cat, User
 
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
@@ -8,7 +10,18 @@ from .serializers import AchievementSerializer, CatSerializer, UserSerializer
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (OwnerOrReadOnly,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter,)
+    pagination_class = CatsPagination
+    filterset_fields = ('color', 'birth_year')
+    search_fields = ('achievements__name', 'owner__username')
+    ordering_fields = ('birth_year',)
+    ordering = ('birth_year',)
+
+    def get_permissions(self):
+        if self.action == 'retrieve':
+            return (ReadOnly(),)
+        return super().get_permissions()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
